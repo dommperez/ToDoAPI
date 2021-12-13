f8e40b12f1bdc8e1bc6c32207937a3cba8f22f7d
dommperez <dperez20002@outlook.com>
2021-12-13 11:15:13 -0600

    to deploy

---
+++ ToDoAPI/ToDoAPI.API/Controllers/ToDoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.DATA.EF;
+using ToDoAPI.API.Models;
+using System.Web.Http.Cors;
+
+namespace ToDoAPI.API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ToDoController : ApiController
+    { 
+        ToDoEntities db = new ToDoEntities();
+
+        //Read 
+        //api/ToDo
+        //public IHttpActionResult GetToDoItems()
+        //{
+        //    List<ToDoItemViewModel> todoItems = db.TodoItems.Include("Category").Select(testc => new ToDoItemViewModel()
+        //    {
+
+        //    }).ToList<ToDoItemViewModel>();
+        //}
+    }
+}
---
+++ ToDoAPI/ToDoAPI.API/Models/CategoriesViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToDoAPI.API.Models
+{
+    public class CategoryViewModels
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual ToDoItemViewModel Category { get; set; }
+    }
+
+    public class ToDoItemViewModel
+    {
+        public int TodoId { get; set; }
+        public string Action { get; set; }
+        public bool Done { get; set; }
+    }
+}
